e84ea0891e80d5e5afc93d23dffa013f80880218
vyctorvguimaraes <vyctor.guimaraes@conexia.com.br>
2022-02-22 00:33:52 -0300

    chave estrangeira

--- curso.sql
+++ curso.sql
@@ -1,18 +1,18 @@
 # Criando banco de dados
-create database curso_mysql;
-
-# Selecionando tabela
-use curso_mysql;
-
-# Criando tabelas
+create database curso_mysql;# Selecionando tabela
+use curso_mysql;# Criando tabelas
 create table clientes(
-	id int not null primary key,
-  nome varchar(45) not null,
-  email varchar(100) not null
-);
-
-create table produtos(
-	id int not null primary key,
+    id int not null primary key,
+    nome varchar(45) not null,
+    email varchar(100) not null
+);create table produtos(
+    id int not null primary key,
     nome varchar(100) not null,
     preco double not null
+);# Chave estrangeira
+create table pedidos(
+    id int primary key,
+    produto_id int not null,
+    quantidade int not null,
+    foreign key(produto_id) references produtos(id)
 );
